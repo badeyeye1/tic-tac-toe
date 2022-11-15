dcc3e92642a4c214bf91b0a2ac930cf4f7466af4
Adebisi Adeyeye <bc.adeyeye@gmail.com>
2022-11-15 11:36:41 +0100

    User can select cross or Naught

--- lib/tic_tac_toe_web/live/games_live/index.ex
+++ lib/tic_tac_toe_web/live/games_live/index.ex
@@ -1,5 +1,6 @@
 defmodule TicTacToeWeb.GamesLive.Index do
   use TicTacToeWeb, :live_view
+  @pieces ["X", "O"]
 
   defmodule Board do
     defstruct [:a1, :b1, :c1, :a2, :b2, :a3, :c2, :b3, :c3]
@@ -8,6 +9,15 @@ defmodule TicTacToeWeb.GamesLive.Index do
   @impl true
   def mount(_params, _session, socket) do
     board = Map.from_struct(%Board{})
-    {:ok, assign(socket, board: board)}
+    {:ok, assign(socket, board: board, select_piece: false, pieces: @pieces, piece: nil)}
+  end
+
+  @impl true
+  def handle_event("select-piece", _, socket) do
+    {:noreply, assign(socket, select_piece: true)}
+  end
+
+  def handle_event("piece-selected", %{"piece" => piece}, socket) do
+    {:noreply, assign(socket, piece: piece, select_piece: false)}
   end
 end
--- test/tic_tac_toe_web/live/games_live_test.exs
+++ test/tic_tac_toe_web/live/games_live_test.exs
@@ -10,7 +10,49 @@ defmodule TicTacToeWeb.GamesLiveTest do
     assert html |> board_cells() |> length() == 9
   end
 
+  test "user can select cross or Naught", %{conn: conn} do
+    {:ok, view, _html} = live(conn, ~p"/")
+
+    assert has_element?(select_piece_button(view))
+
+    open_selection_modal(view)
+
+    assert view |> player_selection_modal() |> has_element?()
+    assert has_element?(player_option(view, "X"))
+    assert has_element?(player_option(view, "O"))
+
+    assert view
+           |> player_option("X")
+           |> render_click() =~ "You are: X"
+
+    refute view |> player_selection_modal() |> has_element?()
+
+    open_selection_modal(view)
+
+    assert view
+           |> player_option("O")
+           |> render_click() =~ "You are: O"
+  end
+
+  defp select_piece_button(view) do
+    element(view, "[data-role='piece-selector-btn']")
+  end
+
+  defp player_selection_modal(view) do
+    element(view, "[data-role='player-option']")
+  end
+
+  defp player_option(view, option) do
+    element(view, "[data-role='player-option-#{option}']")
+  end
+
   defp board_cells(html) do
     Floki.find(html, "#game-board .board-cell")
+  end
+
+  defp open_selection_modal(view) do
+    view
+    |> select_piece_button()
+    |> render_click()
   end
 end
